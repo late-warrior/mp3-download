c326bb55787a7e6993dffe8711c09fb929e44020
Sriram <trnsriram@gmail.com>
2012-07-20 01:14:05 +0530

    Storing music files by default in the currently logged in users Music directory, using the os module's expanduser method for this purpose - os.path.expanduser helps us expand the ~ .  also storing the names of the files as the same as that on the server by using curl's -O switch.  But using this switch prevented me from using the -o switch to store the mp3 file in a particular location, so i will temporarily be storing the mp3 file in the current location before moving it over to the directory specified by the user

--- music-parser.py
+++ music-parser.py
@@ -19,7 +19,7 @@ class MusicParser(HTMLParser):
     def check_if_mp3_link(self,tag,attrs):
         if (tag == 'a'):
             #attrs is a list of name,value pairs
-            link = [value for(key,value) in attrs if(value.endswith('mp3'))]
+            link = [value for(key,value) in attrs if(value.endswith('mp3') and value.strip().startswith('http'))]
             return link
 
     def handle_starttag(self,tag,attrs):
@@ -31,6 +31,7 @@ class MusicParser(HTMLParser):
     def handle_data(self,data):
         link = self.check_if_mp3_link(self.tag,self.attrs)
         if (link != None and len(link) != 0):
+            print "data is %s" % data
             self.song_mp3_link_dict[data] = self.link
 
     #these would serve as the interface methods for this class
@@ -41,18 +42,18 @@ def command_line_parse():
     parser = argparse.ArgumentParser(description="Utility script which either downloads" + 
         "all mp3 files in the link specified by the s and p options or reads the secondary url options from" +
         "a file")
-    group = parser.add_mutually_exclusive_group(required=True)
+
     parser.add_argument('-s','--site',help="web page which contains mp3 songs which you want to download",default="http://tamilmp3world.com")
-    group.add_argument('-p','--path',help="part of the url following the site url",default="")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-p','--path', help="part of the url following the site url",default="")
     group.add_argument('-i','--input',help="file from which we need to get the input, every top level input which we want to" +
     " parse for mp3 urls must be given as a single line in the file.  This option and the -p option are mutually exclusive",default=".")
-    parser.add_argument('-t','--dir', help="directory where we store our downloaded files",default=".")
+    parser.add_argument('-t','--dir', help="directory where we store our downloaded files",default="~/Music")
     args = parser.parse_args()
     return args
     
 def download_music(site,path,filepath):
     parser=MusicParser()
-    print filepath, path
     filepath = os.path.join(filepath,path)
     if (os.path.exists(filepath)):
         print 'Folder : ' + path + ' already exists, so not recreating'
@@ -64,32 +65,34 @@ def download_music(site,path,filepath):
     html = str(data)
     parser.feed(html)
     ua='"User-Agent:Chromium/17.0.963.79"'
-    cmd = 'curl -#v -o {0} -H {1} {2}'
+    cmd = 'curl -L -#v -O -H {0} {1}'
+    #move all mp3 files which are downloaded (currently we blindly move all mp3 files)
+    movecmd = 'mv *.mp3 {0}'.format(filepath)
     song_link_map = parser.get_mp3_map()
     print song_link_map
     print parser.songlinks
     for link in parser.songlinks:
     #for link in song_link_map.keys():
+        link = link.strip()
+        print link
         encodedlink = urllib2.quote(link,safe="/:")
-        #musicpath = os.path.join(filepath.strip(),urllib2.quote(link.split('-')[-1].strip()))
-        musicpath = os.path.join(filepath.strip(),urllib2.quote(link.split('-')[-1].strip()))
-        command = cmd.format(musicpath,ua,encodedlink.strip())
+        command = cmd.format(ua,encodedlink.strip())
         print command
         subprocess.call(command,shell=True)
+    subprocess.call(movecmd,shell=True)
 
 if __name__ == '__main__':
     args = command_line_parse()
-    site = args.site
+    site = args.site.strip()
     if site == None or site == "":
         print "Site name not given, unable to proceed further"
         exit()
 
-    if (args.dir != '.'):
+    if (args.dir != '~/Music'):
         filepath = args.dir
     else:
-        filepath = os.path.abspath('.')
+        filepath = os.path.expanduser(args.dir)
     if (args.input != "."):
-        #f=open(os.path.join(os.path.abspath('.'),'songs-needed'),'r')
         f=open(args.input,'r')
         path_list=f.readlines()
         for path in path_list:
